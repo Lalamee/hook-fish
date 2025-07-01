b62842212488246621826660e9c1ca21e950adba
Lalamee <kurilov6dv@gmail.com>
2025-07-01 23:52:20 +0300

    start start

---
+++ HookAndFish/Assets/_Sourse/UI/Scripts/SettingButton.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingButton : MonoBehaviour
+{
+    [SerializeField] private GameObject SettinMenu;
+    
+    
+}
